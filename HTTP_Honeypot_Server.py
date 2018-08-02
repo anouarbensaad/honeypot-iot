984e54440faa169e0ca9f1af9d332c4c591e880b
Anouar Ben Saad <bensaad.tig@gmail.com>
2018-08-02 18:31:37 +0200

    Update HTTP_Honeypot_Server.py

--- HTTP_Honeypot_Server.py
+++ HTTP_Honeypot_Server.py
@@ -13,12 +13,20 @@ def Main():
  	password = "toor"
  	username = "root"
 	
+	#DATABASE CONNECT
+	
 	con = MySQLdb.connect("localhost", "root", "isetso", "isetsohoney")
 	cursor = con.cursor()
 	
- 	user_in = raw_input('Please Enter username: ')
+	#ROOT , #TOOR
+ 	
+	user_in = raw_input('Please Enter username: ')
  	user_input = raw_input('Please Enter Password: ')
- 	with open('Sys/log/login, PWD','a') as file :
+ 	
+	#Make Directory File and touch file Login, PWD [Sys/log/login, PWD]
+	#Login, PWD : This File For Hacker Trying & URI
+	
+	with open('Sys/log/login, PWD','a') as file :
        		file.write(timestr +'\n' +'LOGIN: ' +str(user_in)+'\n' +'PWD: '+str(user_input)+'\n')
 
  	if  user_input != password or user_in != username  :
@@ -29,14 +37,23 @@ def Main():
 
  	print 'User is logged in!\n' 
  
-
+	#DEFAULT : socket.AF_INET ,socket.SOCK_STREAM
+	
 	s = socket.socket()
 	server_name = ''
+	
+	#Port Listning 999
+	
 	server_port = 999
+	
+	#Real Router Banner , For Scanning Nmap
+	#nmap -sV --script=banner @ip -p999
+	
 	server_banner = """Sagem F@st 2604 ADSL router linux 7 3.49a4G_Topnet
   | banner: \xFF\xFD\x01\xFF\xFD!\xFF\xFB\x01\xFF\xFB\x03FAST2604 ADSL Rout
   |_er (Software Version:3.49a4G_Topnet)\x0D\x0ALogin:
   Service Info: Device: broadband router """
+	
 	s.bind((server_name, server_port))
 	
 	header = ''
@@ -52,6 +69,8 @@ def Main():
 		requested_file = list[1]
                 print ('Method: ', method)
 		
+		#if scanning .. send the server banner.
+		#if data == "http","HTTP/1.0","GET","bind","version","OPTIONS"
 		if (data == 'OPTIONS / HTTP/1.0') :
 			c.send(server_banner) 
 		
@@ -68,6 +87,9 @@ def Main():
 		print  'TIME:',current_time, ' HACKER: ', ip_hacker
 		print  'Bad guy is looking for :',addr, data
 
+		#Make Directory File and touch file Client Data [Sys/log/Client Data]
+		#Client Data : This File For Hacker Infromations
+		
 		with open('Sys/log/Client Data', 'a') as file :
                    file.write(timestr +'Informations :' +str(data)+'\n')
 
@@ -104,7 +126,7 @@ def Main():
 
                   header = 'HTTP/1.1 404 not found \n\n'
  		  response = '<html><body><p>Error 404: not found</p></body></html>'		
-
+		#Make Directory File and touch file File Requested [Sys/log/File Requested]
 		with open('Sys/log/File Requested', 'a') as file :
            	   file.write(timestr +'Body request :' +str(requested_file)+'\n')
 		
